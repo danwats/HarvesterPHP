9e0a65dcc88e197081ce65455ff0d43774a9cf34
Dan Watson <dan.wat@live.com>
2025-10-12 23:46:48 +0100

    first commit

---
+++ src/DNS.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DNS\Harvester;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Output\ConsoleOutput;
+
+
+class DNS {
+    private string $domain;
+    private RecordList $recordList;
+    private bool $showProgress;
+    public array $results = [];
+
+    public function __construct(string $domain, RecordList $records, bool $showProgress = false) {
+        $this->domain = $domain;
+        $this->recordList = $records;
+        $this->showProgress = $showProgress;
+    }
+
+    public function run() {
+        $recordCount = $this->recordList->countTypes();
+        $this->getRecords($recordCount);
+    }
+
+    private function getRecords(int $recordCount) {
+        $records = $this->recordList;
+        $domain = $this->domain;
+
+
+        $tasks = [];
+        foreach($records as $recordList) {
+            foreach($recordList as $record) {
+                foreach($record->type as $recordTypes) {
+                    if ($record->name === "@") {
+                        array_push($tasks, ['hostname' => $domain, 'recordType' => $recordTypes]);
+                    } else {
+                        array_push($tasks, ['hostname' => $record->name . '.' . $domain, 'recordType' => $recordTypes]);
+                    }
+                }
+            } 
+        }
+
+        if ($this->showProgress) {
+            $output = new ConsoleOutput();
+            $progressBar = new ProgressBar($output, $recordCount);
+        }
+        foreach ($tasks as $task) {
+            $result = dns_get_record($task['hostname'], $task['recordType']->toDNS());
+            foreach($result as $records) {
+                if (str_contains($records['host'], $this->domain)) {
+                    $this->results[$task['recordType']->toLetter()][$task['hostname']] = $result;
+                }
+            }
+            if ($this->showProgress) $progressBar->advance();
+        }
+    }
+
+    function json(): string {
+        return json_encode($this->results);
+    }
+
+    function json_pretty(): string {
+        return json_encode($this->results, JSON_PRETTY_PRINT);
+    }
+}
+
